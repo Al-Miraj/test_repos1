6122e3bb48ecf89223c6d7023dbd47e96b8dfdd7
Al-Miraj <khadija.arkhouch@outlook.com>
2023-10-29 11:25:02 +0100

    added fields to Deal class in Deal.cs for potential use in the future

--- Deal.cs
+++ Deal.cs
@@ -3,6 +3,9 @@
     public string Name;
     public string Description;
     public double DiscountFactor;
+    //public bool CurrentlyRunning; will be used by the admin later
+    //public DateOnly FromDate;
+    //public DateOnly ToDate;
     public Deal(string name, string description, double discountFactor)
     {
         Name = name;
