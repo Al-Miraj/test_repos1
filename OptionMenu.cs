36a0dc8b04ace1ca675fb47dbd714d17198a2f98
Lazboy61 <144031531+Lazboy61@users.noreply.github.com>
2023-11-11 14:45:37 +0100

    Update OptionMenu.cs

--- OptionMenu.cs
+++ OptionMenu.cs
@@ -1,58 +1,58 @@
 /*public class Menu //change name?
 {
-    public void RunMenu()
-    {
-        while (true)
-        {
-            Console.Clear();
-            Console.WriteLine("Welcome to the Restaurant Menu:");
-            Console.WriteLine("1. Reservation");
-            Console.WriteLine("2. About Us");
-            Console.WriteLine("3. Contact Us");
-            Console.WriteLine("4. Menu");
-            Console.WriteLine("5. Login");
-            Console.WriteLine("6. Exit");
-            Console.Write("Please select an option (1-6): ");
+   public void RunMenu()
+   {
+       while (true)
+       {
+           Console.Clear();
+           Console.WriteLine("Welcome to the Restaurant Menu:");
+           Console.WriteLine("1. Reservation");
+           Console.WriteLine("2. About Us");
+           Console.WriteLine("3. Contact Us");
+           Console.WriteLine("4. Menu");
+           Console.WriteLine("5. Login");
+           Console.WriteLine("6. Exit");
+           Console.Write("Please select an option (1-6): ");
 
-            string choice = Console.ReadLine();
+           string choice = Console.ReadLine();
 
-            switch (choice)
-            {
-                case "1":
-                    Console.WriteLine("Reservation - Please contact us to make a reservation.");
-                    ReservationSystem sstm = new ReservationSystem();
-                    sstm.SystemRun();
-                    Console.ReadLine();
-                    break;
-                case "2":
-                    About.RestaurantInformation();
-                    Console.ReadLine();
-                    break;
-                case "3":
-                    Console.WriteLine("Contact Us - Get in touch with us.");
-                    Contact.ContactInformation();
-                    Console.ReadLine();
-                    AboutUs.travel();
-                    Console.ReadLine();
-                    break;
-                case "4":
-                    Console.WriteLine("Menu - Check out our delicious dishes.");
-                    FoodMenu.Display();
-                    break;
-                case "5":
-                    Console.WriteLine("Login - Enter your credentials to log in.");
-                    Console.ReadLine();
-                    break;
-                case "6":
-                    Console.WriteLine("Goodbye! Thank you for visiting.");
-                    return;
-                default:
-                    Console.WriteLine("Invalid choice. Please select a valid option.");
-                    Console.ReadLine();
-                    break;
-            }
-        }
-    }
+           switch (choice)
+           {
+               case "1":
+                   Console.WriteLine("Reservation - Please contact us to make a reservation.");
+                   ReservationSystem sstm = new ReservationSystem();
+                   sstm.SystemRun();
+                   Console.ReadLine();
+                   break;
+               case "2":
+                   About.RestaurantInformation();
+                   Console.ReadLine();
+                   break;
+               case "3":
+                   Console.WriteLine("Contact Us - Get in touch with us.");
+                   Contact.ContactInformation();
+                   Console.ReadLine();
+                   AboutUs.travel();
+                   Console.ReadLine();
+                   break;
+               case "4":
+                   Console.WriteLine("Menu - Check out our delicious dishes.");
+                   FoodMenu.Display();
+                   break;
+               case "5":
+                   Console.WriteLine("Login - Enter your credentials to log in.");
+                   Console.ReadLine();
+                   break;
+               case "6":
+                   Console.WriteLine("Goodbye! Thank you for visiting.");
+                   return;
+               default:
+                   Console.WriteLine("Invalid choice. Please select a valid option.");
+                   Console.ReadLine();
+                   break;
+           }
+       }
+   }
 }*/
 
 
@@ -60,7 +60,7 @@ using System;
 
 public class Menu
 {
-    ReservationSystem RS = new ReservationSystem(); //RS is short for Reservation System
+    ReservationSystem RSys = new ReservationSystem(); //RS is short for Reservation System
     private static int selectedOption = 1;
 
     public void RunMenu()
@@ -71,7 +71,7 @@ public class Menu
             Console.Clear();
 
             // Highlight the currently selected option
-            for (int i = 1; i <= 6; i++)
+            for (int i = 1; i <= 7; i++)
             {
                 if (i == selectedOption)
                 {
@@ -98,9 +98,12 @@ public class Menu
                         Console.WriteLine(" Menu");
                         break;
                     case 5:
-                        Console.WriteLine(" Login");
+                        Console.WriteLine(" Deals");
                         break;
                     case 6:
+                        Console.WriteLine(" Login");
+                        break;
+                    case 7:
                         Console.WriteLine(" Exit");
                         break;
                 }
@@ -112,7 +115,7 @@ public class Menu
             {
                 selectedOption--;
             }
-            else if (keyInfo.Key == ConsoleKey.DownArrow && selectedOption < 6)
+            else if (keyInfo.Key == ConsoleKey.DownArrow && selectedOption < 7)
             {
                 selectedOption++;
             }
@@ -130,7 +133,7 @@ public class Menu
         switch (option)
         {
             case 1:
-                RS.RunSystem();
+                RSys.RunSystem();
                 break;
             case 2:
                 About.RestaurantInformation();
@@ -145,9 +148,13 @@ public class Menu
                 FoodMenu.Display();
                 break;
             case 5:
-                Console.WriteLine("Login - Enter your credentials to log in.");
+                Console.WriteLine("Here are all the deals we are currently offering:");
+                Restaurant.DisplayDeals();
                 break;
             case 6:
+                LoginSystem.Start();
+                break;
+            case 7:
                 Console.WriteLine("Goodbye! Thank you for visiting.");
                 Environment.Exit(0);
                 break;
