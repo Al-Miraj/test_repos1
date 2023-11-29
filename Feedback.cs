7f739a7cc380a4f67ec63c1e769148579ce186ce
ManuvonFranquemon <145396958+ManuvonFranquemon@users.noreply.github.com>
2023-11-29 14:56:42 +0100

    Feedback.cs
    In Dashboard:
    begin aan SendFeedback (customers) functie gemaakt, nog geen writing to xml file of xml file aangemaakt. Wel de read function toegevoegd voor Read Feedback (admins)
    In MenuSelector:
    generic list ipv string list gemaakt voor algemeen makkelijker gebruik
    
    En Feedback Class toegevoegd  met rating, email en feedback message

---
+++ Feedback.cs
@@ -0,0 +1,13 @@
+﻿public sealed class Feedback
+{
+    public string Email { get; }
+    public int Rating { get; }
+    public string Message { get; }
+
+    public Feedback(string email, int rating, string message)
+    {
+        Email = email;
+        Rating = rating;
+        Message = message;
+    }
+}
